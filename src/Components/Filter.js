9ef08255cf36d30b92a535fba29921a1198d4783
Shivam164 <shivam-ug20@nsut.ac.in>
2022-08-05 22:58:54 +0530

    some changes in filter

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,23 +1,74 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { FilterContext } from "../Contexts/GlobalState";
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
 
 const Filter = () => {
 
-    const {allState, allCity} = useContext(FilterContext);
+    const {rides, allState, allCity, state, setCity, setState} = useContext(FilterContext);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const [filteredCity, setFilteredCity] = useState(allCity);
+
+    useEffect(() => {
+        const _allCity = [];
+        if(!state){
+            setFilteredCity(allCity);
+            return;
+        }
+        for(let ride = 0;ride < rides.length;ride++){
+            if(rides[ride].state == state){
+                _allCity.push(rides[ride].city);
+            }
+        }
+        console.log("here");
+        setFilteredCity(_allCity);
+    }, [state]);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    var __key = 100;
 
     return (
         <div>
-            <h1>Filters</h1>
-            <select name = "State">
-                {allState && allState.map(state => (
-                    <option value = {state} key = {state}>{state}</option>
-                ))}
-            </select>
-            <select name = "City">
-                {allCity && allCity.map(city => (
-                    <option value = {city} key = {city}>{city}</option>
-                ))}
-            </select>
+            <Button
+                id="demo-positioned-button"
+                aria-controls={open ? 'demo-positioned-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+                sx = {{
+                    color :'#ffffff'
+                }}
+            >
+                Filters
+            </Button>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                'aria-labelledby': 'basic-button',
+                }}
+            >
+                <select onChange = {e => setState(e.target.value)}>
+                    {allState && allState.map(state => (
+                        <option value = {state} key = {state} className="state">{state}</option>
+                    ))}
+                </select>
+                {filteredCity && <select onChange = {e => setCity(e.target.value)}>
+                    {filteredCity.map(city => (
+                        <option value = {city} key = {__key++} className="city" >{city}</option>
+                    ))}
+                </select>}
+            </Menu>
         </div>
     )
 }
